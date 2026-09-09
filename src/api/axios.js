aca9f498a8f974fb0479ebbb7ffd6de2664218ec
actionarun <actionofak@gmail.com>
2026-09-09 16:35:25 +0530

    browse url change

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
-});
+   baseURL: "https://notesapp-backend-lps3.onrender.com/api",
+   }); 
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
